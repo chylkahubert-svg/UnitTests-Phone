fb4b192c729baef41bb26dcbeb89ad99b3542689
Hubert Chylka <chylkahubert@gmail.com>
2026-03-05 13:08:43 +0100

    konstruktor dane poprawne

--- ConsoleApp1/TestProject1/Test1.cs
+++ ConsoleApp1/TestProject1/Test1.cs
@@ -1,11 +1,22 @@
-﻿namespace TestProject1
+﻿using ClassLibrary;
+
+namespace TestProject1
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Konstruktor_DanePoprawne()
         {
+            string owner = "John Doe";
+            string phoneNumber = "123456789";
+
+            Phone phone = new(owner, phoneNumber);
+
+            Assert.AreEqual(owner, phone.Owner);
+            Assert.AreEqual(phoneNumber, phone.PhoneNumber);
         }
+
+       
     }
 }
